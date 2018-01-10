3dfcd94ea3a2b00c141ce36180f5ff19737c8c9d
Becky <becky@degreed.com>
2018-01-10 14:07:28 -0800

    Task: bootstrapping

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
+    selector: 'app-root',
+    styleUrls: ['app.component.scss'],
+    template: `
+        <div class="app">
+            {{ title }}
+        </div>
+    `
 })
 export class AppComponent {
+    title: string;
 
+    constructor() {
+        this.title = 'Ultimate Angular';
+    }
 }
