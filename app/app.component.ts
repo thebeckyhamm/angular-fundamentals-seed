fef4c9c146481b6d5c5211ebdd83bb5089c2642b
Becky <becky@degreed.com>
2018-02-15 10:20:00 -0800

    Task: ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,46 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+    id: number,
+    fullname: string,
+    checkedIn: boolean
+}
+
 @Component({
     selector: 'app-root',
     styleUrls: ['app.component.scss'],
     template: `
         <div class="app">
-            <input type="text" [value]="name" (input)="handleChange($event.target.value)">
-
-
-            <div *ngIf="name.length">Searching for ...{{name}}</div>
+            <h3>Airline Passengers</h3>
+            <ul>
+                <li *ngFor="let passenger of passengers; let i = index;">
+                    {{i}}: {{ passenger.fullname }}
+                </li>
+            </ul>
         </div>
     `
 })
 export class AppComponent {
-    name: string = "";
-    handleChange(value: string) {
-        this.name = value;
-    }
+    passengers: Passenger[] = [{
+        id: 1,
+        fullname: 'Stephen',
+        checkedIn: true
+    }, {
+        id: 2,
+        fullname: 'Rose',
+        checkedIn: false
+    }, {
+        id: 3,
+        fullname: 'James',
+        checkedIn: true
+    }, {
+        id: 4,
+        fullname: 'Louise',
+        checkedIn: true
+    }, {
+        id: 5,
+        fullname: 'Tina',
+        checkedIn: false
+    }];
  
 }
